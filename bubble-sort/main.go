40c2e7ddda657502ad4fd650317e27047f91147e
stevanstev <stevanussteven7@gmail.com>
2020-04-21 19:04:30 +0700

    Adding Test Case

---
+++ bubble-sort/main.go
@@ -0,0 +1,42 @@
+package main
+
+//BubbleSort function
+func BubbleSort(n []int, sortType string) []int {
+	var result []int
+	var validateSortType bool
+	temp := 0
+	sliceLen := len(n)
+	selectedIndex := 0
+
+	for i := 0; i < sliceLen; i++ {
+		sizeSlice := len(n)
+		for j := 0; j < sizeSlice; j++ {
+			//If Ascending
+			validateSortType = temp < n[j]
+
+			//If Descending
+			if sortType != "desc" {
+				validateSortType = temp > n[j]
+			}
+
+			//If start index
+			if j == 0 || validateSortType {
+				temp = n[j]
+				//Get the index of selected number
+				selectedIndex = j
+				continue
+			}
+
+		}
+
+		//Remove the smallest number in slice
+		n = append(n[:selectedIndex], n[selectedIndex+1:]...)
+		result = append(result, temp)
+	}
+
+	return result
+}
+
+func main() {
+	//
+}
---
+++ bubble-sort/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	unsortedData := []int{0, 4, 5, 1, 6, 23, 4, 2}
+	expectedResult := []int{0, 1, 2, 4, 4, 5, 6, 23}
+	result := BubbleSort(unsortedData, "asc")
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Error("Test Case Invalid")
+	}
+
+	t.Log("Test Case Valid")
+}
+
+func benchmarkBubbleSort(n []int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BubbleSort(n, "")
+	}
+}
+
+func BenchmarkBubbleSort2(b *testing.B) { benchmarkBubbleSort([]int{5, 2}, b) }
+func BenchmarkBubbleSort3(b *testing.B) { benchmarkBubbleSort([]int{5, 2, 3}, b) }
+func BenchmarkBubbleSort4(b *testing.B) { benchmarkBubbleSort([]int{5, 2, 10, 12}, b) }
+func BenchmarkBubbleSort5(b *testing.B) { benchmarkBubbleSort([]int{5, 2, 4, 1, 2}, b) }
